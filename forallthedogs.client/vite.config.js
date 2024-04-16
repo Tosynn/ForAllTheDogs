3db87d5dc3b3e42883d828f7d60b18b344377644
Tosin Adewale <tosinadewale01@gmail.com>
2024-04-16 17:01:43 +0100

    Fixed API Port Issue.

--- forallthedogs.client/vite.config.js
+++ forallthedogs.client/vite.config.js
@@ -31,7 +31,7 @@ if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
 }
 
 const target = env.ASPNETCORE_HTTPS_PORT ? `https://localhost:${env.ASPNETCORE_HTTPS_PORT}` :
-    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:5001';
+    env.ASPNETCORE_URLS ? env.ASPNETCORE_URLS.split(';')[0] : 'https://localhost:7290';
 
 // https://vitejs.dev/config/
 export default defineConfig({
