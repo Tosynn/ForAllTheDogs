b66055075c8d6a65049ab1e68763aa3fdfbd9be4
Tosin Adewale <tosinadewale01@gmail.com>
2024-04-19 22:24:26 +0100

    Minor edit on FoodModel

--- ForAllTheDogs.Server/Models/FoodModel.cs
+++ ForAllTheDogs.Server/Models/FoodModel.cs
@@ -5,7 +5,6 @@
         public int foodId { get; set; }
         public string foodName { get; set; } = string.Empty;
         public string foodDescription { get; set;} = string.Empty;
-        public string foodType { get; set;}= string.Empty;
 
     }
 }
