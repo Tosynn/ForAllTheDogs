58479f92fc1934bcbd05f3b63832cff45770421a
Tosin Adewale <tosinadewale01@gmail.com>
2024-05-13 22:36:22 +0100

    Frontend build initial commit

--- forallthedogs.client/src/App.jsx
+++ forallthedogs.client/src/App.jsx
@@ -1,4 +1,3 @@
-import { useEffect, useState } from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
